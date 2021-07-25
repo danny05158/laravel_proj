c59e5f7e9b7b59008715a365adc456bdbec36789
Daniel Casasola <casasolad86@gmail.com>
2021-07-25 11:02:15 -0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 Route::post('/dashboard', [App\Http\Controllers\DashboardController::class, 'main'])->name('main');
 
-// Route::get('/main', [App\Http\Controllers\DashboardController::class, 'main'])->name('main');
-
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
